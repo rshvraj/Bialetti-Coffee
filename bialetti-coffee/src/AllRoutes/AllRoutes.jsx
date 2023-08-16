855974e2631aad89b871baa08011f8cbb19e5f5c
rshvraj <33702025+rshvraj@users.noreply.github.com>
2023-08-16 16:22:03 +0530

    cart update

--- bialetti-coffee/src/AllRoutes/AllRoutes.jsx
+++ bialetti-coffee/src/AllRoutes/AllRoutes.jsx
@@ -3,13 +3,16 @@ import {Routes,Route} from 'react-router-dom';
 import HomePage from '../Home/HomePage';
 import Cart from '../Components/Cart/Cart';
 import Product from "../Components/product_page/Product"
+import Singup from "../Components/product_page/Login-SignUp/Signup"
+import Login from "../Components/product_page/Login-SignUp/Login"
 
 const AllRoutes = () => {
   return (
     <Routes>
         <Route path='/' element={<HomePage/>} />
-        {/* <Route path='/login' element={<Login />}/>
-        <Route path='/signup' element={<SignUp/>}/> */}
+        <Route path='/login' element={<Login/>}/>
+
+        <Route path='/signup' element={<Singup/>}/>
         <Route path='/cart' element={<Cart/>}/>
         <Route path='/products' element={<Product/>} />
     </Routes>
